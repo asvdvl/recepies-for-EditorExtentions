7189e03e643882d662c290a59a526ba7033f7f3e
asvdev <57688070+asvdvl@users.noreply.github.com>
2024-03-24 00:06:23 +0300

    simple docs for utils file

--- utils.lua
+++ utils.lua
@@ -5,12 +5,17 @@ local all_technologies = data.raw.technology
 local all_recipes = data.raw.recipe
 local local_defines = {}
 
---need for checking that we are not working with an item from the EE mod
+--- need for checking that we are not working with an item from the EE mod
+--- @param item_name string input item name for checking
+--- @return table|number|nil
 function utils.is_item_NOT_from_EE(item_name)
     return defines.recipes[item_name] or string.sub(item_name, 1, 3) ~= "ee-"
 end
 
---allows to specify some "code" in a string that is executed here. for example (25, '^2') -> 625
+--- allows to specify some "code" in a string that is executed here. for example (25, '^2') -> 625
+--- @param x integer|string
+--- @param multiplier integer|string
+--- @return integer
 function utils.perfom_math_from_string(x, multiplier)
     local func, errorMsg = load("return "..x..multiplier)
     if func then
@@ -19,11 +24,9 @@ function utils.perfom_math_from_string(x, multiplier)
             return result
         else
             error('error when executing expression ('..x..', '..multiplier..') by reason '..result)
-            return x
         end
     else
         error('error when executing expression ('..x..', '..multiplier..') by reason '..errorMsg)
-        return x
     end
 end
 
@@ -32,9 +35,11 @@ end
 local tech_cache = {}
 
 --the task is to find the “unlock-recipe” type from the effects of technology, and in it the desired item in the recipe
-function utils.is_item_has_technology(item_name)
-    if tech_cache[item_name] then
-        return tech_cache[item_name]    --I don’t know for sure whether it will be needed more than once, I left it just in case
+--- @param proto_name string input item(or entity) name for checking
+--- @return table<string|boolean, string>|boolean {tech-name, recipe-name}; true means item enabled by default
+function utils.is_item_has_technology(proto_name)
+    if tech_cache[proto_name] then
+        return tech_cache[proto_name]    --I don’t know for sure whether it will be needed more than once, I left it just in case
     end
 
     for tech_name, tech in pairs(all_technologies) do
@@ -42,14 +47,14 @@ function utils.is_item_has_technology(item_name)
             for _, effect in pairs(tech.effects) do
                 if effect.type == "unlock-recipe" then
                     local recipe = all_recipes[effect.recipe]
-                    if recipe.result and recipe.result == item_name then
-                        tech_cache[item_name] = {tech_name, recipe.name}
-                        return tech_cache[item_name]
+                    if recipe.result and recipe.result == proto_name then
+                        tech_cache[proto_name] = {tech_name, recipe.name}
+                        return tech_cache[proto_name]
                     elseif recipe.results then
                         for _, results in pairs(recipe.results) do
-                            if results.name == item_name then
-                                tech_cache[item_name] = {tech_name, recipe.name}
-                                return tech_cache[item_name]
+                            if results.name == proto_name then
+                                tech_cache[proto_name] = {tech_name, recipe.name}
+                                return tech_cache[proto_name]
                             end
                         end
                     end
@@ -60,22 +65,22 @@ function utils.is_item_has_technology(item_name)
 
     --The game has items that are enabled by default
     --{false, item_name} because there is no technology but there is a recipe
-    if all_recipes[item_name] and (all_recipes[item_name].enabled or type(all_recipes[item_name].enabled) == "nil") then  --WTF?! Why do some recipes that are pre-enabled have enabled = true and some do not?!
-        tech_cache[item_name] = {false, item_name}
-        return tech_cache[item_name]
+    if all_recipes[proto_name] and (all_recipes[proto_name].enabled or type(all_recipes[proto_name].enabled) == "nil") then  --WTF?! Why do some recipes that are pre-enabled have enabled = true and some do not?!
+        tech_cache[proto_name] = {false, proto_name}
+        return tech_cache[proto_name]
     else
         --hello wube, i love you...
         --some recipes have a different name than the result,
         --I'm just wondering how the game determines the amount of raw resources in runtime? builds a table of items->recipes where is it?
         for _, recipe in pairs(all_recipes) do
-            if recipe.result == item_name and (recipe.enabled or type(recipe.enabled) == "nil") then
-                tech_cache[item_name] = {false, item_name}
-                return tech_cache[item_name]
+            if recipe.result == proto_name and (recipe.enabled or type(recipe.enabled) == "nil") then
+                tech_cache[proto_name] = {false, proto_name}
+                return tech_cache[proto_name]
             elseif recipe.results and (recipe.enabled or type(recipe.enabled) == "nil") then
                 for _, results in pairs(recipe.results) do
-                    if results.name == item_name then
-                        tech_cache[item_name] = {false, item_name}
-                        return tech_cache[item_name]
+                    if results.name == proto_name then
+                        tech_cache[proto_name] = {false, proto_name}
+                        return tech_cache[proto_name]
                     end
                 end
             end
@@ -85,6 +90,11 @@ function utils.is_item_has_technology(item_name)
     return false
 end
 
+--- small function to select from 2 tables
+--- @param value integer
+--- @param pos_data table
+--- @param neg_data table
+--- @return table|nil
 function utils.get_right_table_by_value(value, pos_data, neg_data)
     if value >= 0 then
         return pos_data
@@ -131,6 +141,8 @@ local function find_prototype_category_logic(category_name, category_data, item_
     end
 end
 
+--- @param item_name string input item name for find category
+--- @return string|nil category-name
 function utils.find_prototype_category(item_name)
     if fpc_cache[item_name] then
         return fpc_cache[item_name][1]  --here return only 1 
@@ -152,6 +164,10 @@ function utils.find_prototype_category(item_name)
     end
 end
 
+--- функция для разделения троки вида "table/subtable" -> {"table", "subtable"}
+--- @param str string input string
+--- @param sep string separator
+--- @return table 
 function utils.split(str, sep)
     local result = {}
     for token in string.gmatch(str, "([^" .. sep .. "]+)") do
@@ -164,6 +180,9 @@ end
     a function that returns the "sum" of properties for a specified entity,
     to determine how the entity's properties differ from the properties of another entity
 ]]
+--- @param search_rows table table of string from defines
+--- @param item table working item
+--- @return integer|number 
 function utils.find_diff_value(search_rows, item)
     local totalSum = 0
     local value = 0
@@ -200,6 +219,10 @@ function utils.find_diff_value(search_rows, item)
     return totalSum
 end
 
+--- checks that the table is sorted using the comp function
+--- @param array table
+--- @param comp function(a, b): boolean
+--- @return boolean
 function utils.is_sorted(array, comp)
     if #array == 1 then
         return true
