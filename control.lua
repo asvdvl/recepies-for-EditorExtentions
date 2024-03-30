3546d319e0187d0e8482cd809be23e5b0f3b45f9
asvdev <57688070+asvdvl@users.noreply.github.com>
2024-03-30 19:29:16 +0300

    fixing accidentally enabled recipes in existing world

--- control.lua
+++ control.lua
@@ -7,10 +7,12 @@ local function message_handler()
         return
     end
     local message = ""  --{""}
-    for key in pairs(recipes) do
-        local recipe_proto, tech_proto = game.recipe_prototypes[key], game.technology_prototypes[tech_prefix..key]
+    local fixed_recipes = 0
+    for recipe in pairs(recipes) do
+        local recipe_proto, tech_proto = game.recipe_prototypes[recipe], game.technology_prototypes[tech_prefix..recipe]
+
         if not (recipe_proto and recipe_proto.enabled) and (tech_proto and not tech_proto.enabled) then
-            message = message..'`'..key..'`, '
+            message = message..'`'..recipe..'`, '
             --disabled because I don’t want to deal with the problem of the translations limit yet
             --[[table.insert(message, key)
             if not (recipe_proto and recipe_proto.enabled) then
@@ -21,11 +23,26 @@ local function message_handler()
                 table.insert(message, {"message.no-tech"})
             end
             table.insert(message, ', ')]]
+        else
+            --blocking recipes enabled without technology
+            for _, force in pairs(game.forces) do
+                if not force.technologies[tech_proto.name].researched and force.recipes[recipe_proto.name].enabled then
+                    --[[local tech = force.technologies[tech_proto.name]
+                    tech.researched=true
+                    tech.researched=false]]     --one way to solve the problem
+                    force.recipes[recipe_proto.name].enabled = false
+                    fixed_recipes = fixed_recipes + 1
+                end
+            end
         end
     end
     if #message > 1 then
-        game.print({"", {"message.startup-message", message}})
-        log({"", {"message.startup-message", message}})
+        game.print({"", {"rfEE_message.startup-message", message}})
+        log({"", {"rfEE_message.startup-message", message}})
+    end
+    if fixed_recipes > 0 then
+        game.print({"", {"rfEE_message.fixed_recipes", fixed_recipes}})
+        log({"", {"rfEE_message.fixed_recipes", fixed_recipes}})
     end
     global.message_was_shown = true
 end
